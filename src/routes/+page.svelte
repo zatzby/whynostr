fcb5d16d5ac67f9c97e568fb02ea5f34fdc9d247
zach <128881293+zatzby@users.noreply.github.com>
2023-07-27 23:18:49 -0700

    Update +page.svelte
    added words to typewriter function, and added links within paragraphs to other pages.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,9 @@
         if(browser) {
             const typewriter = new Typed("#typewriter", {
                 strings: [
+                    'decentralized',
+                    'different',
+                    'connective',
                     'ours',
                     'fun',
                     'free',
@@ -34,9 +37,9 @@
 
 <h1 class="flex flex-col md:flex-row"><span>Nostr is... </span><span><span id="typewriter" class="text-purple-700"></span></span></h1>
 <div>
-    <p>Nostr is an emerging network built on a decentralized protocol, where anyone can easily join and immediately begin interacting with a global community of individuals.</p>
+    <p>Nostr is an emerging network built on a decentralized protocol, where anyone can easily <a href="/join">join</a> and immediately begin interacting with a global community of individuals.</p>
 
-    <p>With a few simple clicks, you generate a public and private key - think of it like a username and password - that allow you to access a growing ecosystem of applications.</p>
+    <p>With a few simple clicks, you generate a public and private key - think of it like a username and password - that allow you to <a href="/explore">explore</a> a growing ecosystem of applications.</p>
 
     <p>With Nostr, the power is returned to the individual. Your journey is not controlled by algorithms, riddled with unnecessary and irrelevant ads, but is up to YOU. <a href="/learn">Learn</a> more about the protocol and join the fun!</p>
 </div>
