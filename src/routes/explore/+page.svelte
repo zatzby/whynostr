ff88614962e8928e34a67f59bc203e9abc89ad5c
zatzby <128881293+zatzby@users.noreply.github.com>
2023-07-29 19:35:48 -0700

    Update +page.svelte

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,1 +1,31 @@
-<h1>Explore</h1>
+<script>
+    import { List, Li, A } from 'flowbite-svelte';
+  </script>
+  
+  <h1>Explore</h1>
+
+  <List tag="ul" list="none" class="flex flex-wrap justify-center items-center mb-6">
+    <Li>
+      <A href="/" class="mr-4 hover:underline md:mr-6" textColor="text-gray-700 dark:text-white">Audio</A>
+    </Li>
+    <Li>
+      <A href="/" class="mr-4 hover:underline md:mr-6" textColor="text-gray-700 dark:text-white">Games</A>
+    </Li>
+    <Li>
+      <A href="/" class="mr-4 hover:underline md:mr-6" textColor="text-gray-700 dark:text-white">Media Hosting</A>
+    </Li>
+    <Li>
+      <A href="/" class="mr-4 hover:underline md:mr-6" textColor="text-gray-700 dark:text-white">Newly Added</A>
+    </Li>
+    <Li>
+      <A href="/" class="mr-4 hover:underline md:mr-6" textColor="text-gray-700 dark:text-white"
+        >Relays</A>
+    </Li>
+    <Li>
+      <A href="/" class="mr-4 hover:underline md:mr-6" textColor="text-gray-700 dark:text-white">Social</A>
+    </Li>
+    <Li>
+        <A href="/" class="mr-4 hover:underline md:mr-6"
+        textColor="text-gray-700 dark:text-white">Tools</A>
+    </Li>
+  </List>
