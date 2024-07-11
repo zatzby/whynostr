80430dd38e2e5c41a97ac0d3c722b44e26c4ceec
zach <128881293+zatzby@users.noreply.github.com>
2024-07-10 20:20:22 -0700

    fixed join

---
+++ src/routes/join/+page.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    import { joinLinks } from "$lib/data/join";
+  </script>
+  
+  <h1>Find your client</h1>
+  
+  {#each joinLinks as linkList}
+    <h2>{linkList.title}</h2>
+    <ul>
+        {#each linkList.links as link}
+        <li>
+            <a href={link.url} target="_blank">{link.title}</a> &ndash; {link.description}
+        </li>
+        {/each}
+    </ul>
+  {/each}
